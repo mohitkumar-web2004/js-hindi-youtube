1ca3513a99b5fe1bf2973da81aa1913327505308
mohitkumar-web2004 <mohitkumar07761@gmail.com>
2026-09-25 08:50:32 +0000

    conversion ofdatatype

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false
+
+//   console.log(typeof score);
+//   console.log(typeof (score));
+
+  let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber);
+//   console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abs" => NaN 
+// true => 1; false => 0
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+//  "" => false
+//  "mohit" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
